4445231284c83d65278a7a6bf5dfd57822dd6500
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-09-15 18:21:07 -0700

    terminado section elige-tech

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionIconosComponent } from './section-iconos/section-iconos.component';
 import { SobreTechproComponent } from './sobre-techpro/sobre-techpro.component';
+import { EligeTechComponent } from './elige-tech/elige-tech.component';
 
 
 
 @NgModule({
   declarations: [
     SectionIconosComponent,
-    SobreTechproComponent
+    SobreTechproComponent,
+    EligeTechComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SectionIconosComponent,
-    SobreTechproComponent
+    SobreTechproComponent,
+    EligeTechComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/elige-tech/elige-tech.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elige-tech',
+  templateUrl: './elige-tech.component.html',
+  styleUrls: ['./elige-tech.component.css']
+})
+export class EligeTechComponent {
+  backgroundImageGradientUrl = 'url("/assets/img/modelo-x.svg")';
+  backgroundImageGradientUrlY = 'url("/assets/img/modelo-y.svg")';
+  backgroundImageGradientUrlZ = 'url("/assets/img/modelo-z.svg")';
+}
